3785657e8cc61d444c4ba0b53acace7d5616a6af
BATIKAN BORA ORMANCI <batikanor@gmail.com>
2024-10-26 19:34:46 +0200

    Make cv download automatic

--- src/app/cv/en/page.js
+++ src/app/cv/en/page.js
@@ -1,15 +1,19 @@
 /* eslint-disable react/no-unescaped-entities */
-
 // src/app/cv/en/page.js
+"use client"
+import { useEffect } from 'react';
+
 export default function CvRedirect() {
-    if (typeof window !== 'undefined') {
-        window.location.href = 'https://docs.google.com/document/d/1ZVQcdNvOR46HBUCtVy6XKFOXocRYL9-TR_LLrRfJ2T8/export?format=pdf';
-    }
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            window.location.href = 'https://docs.google.com/document/d/1ZVQcdNvOR46HBUCtVy6XKFOXocRYL9-TR_LLrRfJ2T8/export?format=pdf';
+        }
+    }, []); // Empty dependency array to ensure this runs only once on component mount
 
     return (
         <div>
-            <p>Downloading batikan's most up-to-date cv...</p>
-            <p>If download hasn't started, <a href="https://docs.google.com/document/d/1ZVQcdNvOR46HBUCtVy6XKFOXocRYL9-TR_LLrRfJ2T8/export?format=pdf">click here</a>.</p>
+            <p>Downloading batikan's most up-to-date CV...</p>
+            <p>If the download hasn't started, <a href="https://docs.google.com/document/d/1ZVQcdNvOR46HBUCtVy6XKFOXocRYL9-TR_LLrRfJ2T8/export?format=pdf">click here</a>.</p>
         </div>
     );
 }
