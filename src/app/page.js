ab5330e2f21d97fe12d877bebfc6a61238e7e3b0
BATIKAN BORA ORMANCI <batikanor@gmail.com>
2024-10-27 22:43:49 +0100

    Add close button to hover info windows

--- src/app/page.js
+++ src/app/page.js
@@ -969,8 +969,15 @@ export default function Home() {
               {hoveredMarker.label}
             </h2>
             <p className="mt-2" dangerouslySetInnerHTML={{ __html: hoveredMarker.description }} />
+            <button
+              onClick={() => setHoveredMarker(null)}
+              className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+            >
+              Close
+            </button>
           </div>
         )}
+
 
 
         {/* Modal Popup for Marker Click */}
